7b6799c253a5434a583bc029a2d1be8968eec2fb
Tudda <trezzt0224@gmail.com>
2023-07-18 20:46:17 -0500

    adding buttons abcd elements

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 // imput all of the HTML elements
 var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var buttonD = document.getElementById("d");
